7c28f7e423bdbabb2cea3c3098c3c5d28a41255f
영원 최 <fnul1004@gmail.com>
2024-12-19 17:53:03 +0900

    초기 커밋.

---
+++ CSketcherView/CSketcherView.h
@@ -0,0 +1,43 @@
+﻿
+// CSketcherView.h: CSketcherView 응용 프로그램의 기본 헤더 파일
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // 주 기호입니다.
+
+
+// CCSketcherViewApp:
+// 이 클래스의 구현에 대해서는 CSketcherView.cpp을(를) 참조하세요.
+//
+
+class CCSketcherViewApp : public CWinAppEx
+{
+public:
+	CCSketcherViewApp() noexcept;
+
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 구현입니다.
+	BOOL  m_bHiColorIcons;
+
+	virtual void PreLoadState();
+	virtual void LoadCustomState();
+	virtual void SaveCustomState();
+
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+	afx_msg void OnColorBlack();
+	afx_msg void OnColorBlue();
+	afx_msg void OnColorGreen();
+	afx_msg void OnColorRed();
+};
+
+extern CCSketcherViewApp theApp;
